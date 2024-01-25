b17c64ca0c036760126c86eb6fff0b2f1b3f11c7
Zhehao Mi <zhehaomi@microsoft.com>
2024-01-25 21:36:12 +0800

    user decorator

---
+++ src/common/decorators/user.decorator.ts
@@ -0,0 +1,12 @@
+import type { ExecutionContext } from "@nestjs/common";
+import { createParamDecorator } from "@nestjs/common";
+
+import type { IRequestWithSession } from "@/auth/auth.middleware";
+
+/**
+ * See auth/auth.middleware.ts for request.session
+ */
+export const CurrentUser = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
+    const request: IRequestWithSession = ctx.switchToHttp().getRequest();
+    return request.session?.user;
+});
