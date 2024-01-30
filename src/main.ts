cd99dce98f2876a08039391be244f935b36e6815
Zhehao Mi <zhehaomi@microsoft.com>
2024-01-30 19:08:11 +0800

    use open api

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
 import { Logger, ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { json as expressJSON } from "express";
 
 import { AppModule } from "@/app.module";
 import { ValidationErrorException } from "@/common/exception/validation-error.exception";
 import { ConfigService } from "@/config/config.service";
+import { ErrorFilter } from "@/error.filter";
 
 async function bootstrapAsync() {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const packageInfo = require("../package.json");
+
+    Logger.log(`Starting X5Plan Blog version ${packageInfo.version}`, "Bootstrap");
+
     const app = await NestFactory.create(AppModule);
 
     app.setGlobalPrefix("api");
+    app.useGlobalFilters(app.get(ErrorFilter));
     app.useGlobalPipes(
         new ValidationPipe({
             always: true,
@@ -21,6 +29,17 @@ async function bootstrapAsync() {
     );
     app.use(expressJSON({ limit: "50mb" }));
 
+    const document = SwaggerModule.createDocument(
+        app,
+        new DocumentBuilder()
+            .setTitle("X5Plan Blog")
+            .setDescription(packageInfo.description)
+            .setVersion(packageInfo.version)
+            .addBearerAuth()
+            .build(),
+    );
+    SwaggerModule.setup("/docs", app, document);
+
     const configService = app.get(ConfigService);
 
     await app.listen(configService.config.server.port, configService.config.server.hostname);
@@ -30,4 +49,7 @@ async function bootstrapAsync() {
         "Bootstrap",
     );
 }
-bootstrapAsync();
+
+bootstrapAsync().catch((e) => {
+    Logger.error(e, "App");
+});
