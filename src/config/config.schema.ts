cb9e99a141937506df570a209bf33f6850bb2107
Antares <antares.mi@outlook.com>
2024-02-09 02:17:06 +0800

    Fix config schema (#11)

--- src/config/config.schema.ts
+++ src/config/config.schema.ts
@@ -14,6 +14,7 @@ import {
     ValidateNested,
 } from "class-validator";
 
+import { IsHostname } from "@/common/validators/hostname";
 import { IsPortNumber } from "@/common/validators/port-number";
 import { IsUsername } from "@/common/validators/username";
 
@@ -26,7 +27,9 @@ class ServerConfig {
 }
 
 class DatabaseConfig {
-    @IsIP()
+    @IsHostname({
+        require_tld: false,
+    })
     public readonly host: string;
 
     @IsPortNumber()
